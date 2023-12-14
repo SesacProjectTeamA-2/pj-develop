15569bd042c5bb023169d047e7e0738ad68d31f9
chitty12 <cty12angel@naver.com>
2023-12-14 13:38:33 +0900

    feat/socketToken: 소켓연결시 토큰검증

---
+++ server/middlewares/authSocket.js
@@ -0,0 +1,39 @@
+const jwt = require('../modules/jwt');
+// const MSG = require('../modules/responseMessage');
+// const CODE = require('../modules/statusCode');
+// const util = require('../modules/util');
+const TOKEN_EXPIRED = -3;
+const TOKEN_INVALID = -2;
+
+const authSocketUtil = {
+  checkToken: async (socket, next) => {
+    try {
+      var token = socket.handshake.headers.authorization.split(' ')[1];
+
+      // 토큰 없음
+      if (!token) return next(new Error('토큰 없음'));
+
+      // decode
+      const user = await jwt.verify(token);
+
+      // 유효기간 만료
+      if (user === jwtUtil.TOKEN_EXPIRED)
+        return next(new Error('유효기간 만료된 토큰'));
+
+      // 유효하지 않는 토큰
+      if (user === jwtUtil.TOKEN_INVALID)
+        return next(new Error('유효하지 않는 토큰'));
+
+      // 토근 : undefined
+      if (user.uSeq === undefined) return next(new Error('토큰 : undefined'));
+
+      socket.uSeq = user.uSeq;
+      socket.uName = user.uName;
+      next();
+    } catch {
+      return res.send({ error: '토큰 확인 중 서버 에러' });
+    }
+  },
+};
+
+module.exports = authSocketUtil;
