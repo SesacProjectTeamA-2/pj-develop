117ea325b2948013149a6e358db6157440b2904a
saywhat <95282021+loveflora@users.noreply.github.com>
2023-12-14 14:42:57 +0900

    refactor: 채팅 로그인 확인

--- client/src/components/common/Alarm.tsx
+++ client/src/components/common/Alarm.tsx
@@ -24,7 +24,10 @@ export default function Alarm({ alarmHandler }: any) {
                     <div className="notification">
                         <div className="circle"></div>
                         <span className="time">9:24 AM</span>
-                        <svg
+                        <button>읽음</button>
+
+                        {/* hover 시, 읽음 표시 */}
+                        {/* <svg
                             fill="#4264dd9e"
                             viewBox="0 0 16 16"
                             height="1.4em"
@@ -33,7 +36,7 @@ export default function Alarm({ alarmHandler }: any) {
                         >
                             <path d="M16 8A8 8 0 110 8a8 8 0 0116 0zm-3.97-3.03a.75.75 0 00-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 00-1.06 1.06L6.97 11.03a.75.75 0 001.079-.02l3.992-4.99a.75.75 0 00-.01-1.05z" />
                         </svg>
-                        <span className="check-text">읽음 표시</span>
+                        <span className="check-text">읽음 표시</span> */}
                         <p>
                             <b>John Walker</b> posted a photo on your wall.
                         </p>
@@ -42,7 +45,7 @@ export default function Alarm({ alarmHandler }: any) {
                         <div className="circle"></div>
 
                         <span className="time">8:19 AM</span>
-                        <button>읽음 표시</button>
+                        <button>읽음</button>
                         <p>
                             <b>Alice Parker</b> commented your last post.
                         </p>
