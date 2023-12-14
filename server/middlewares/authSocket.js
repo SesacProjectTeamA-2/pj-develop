553377e085b8de0ec5e4c40562cf4acdcdafcd46
chitty12 <cty12angel@naver.com>
2023-12-14 14:05:13 +0900

    fix/socketLogin

--- server/middlewares/authSocket.js
+++ server/middlewares/authSocket.js
@@ -9,29 +9,28 @@ const authSocketUtil = {
   checkToken: async (socket, next) => {
     try {
       var token = socket.handshake.headers.authorization.split(' ')[1];
-
+      console.log('>>>>>>>>>>>>>>>>>', token);
       // 토큰 없음
-      if (!token) return next(new Error('토큰 없음'));
+      if (!token) return next('토큰 없음');
 
       // decode
       const user = await jwt.verify(token);
 
       // 유효기간 만료
-      if (user === jwtUtil.TOKEN_EXPIRED)
-        return next(new Error('유효기간 만료된 토큰'));
+      if (user === jwt.TOKEN_EXPIRED) return next('유효기간 만료된 토큰');
 
       // 유효하지 않는 토큰
-      if (user === jwtUtil.TOKEN_INVALID)
-        return next(new Error('유효하지 않는 토큰'));
+      if (user === jwt.TOKEN_INVALID) return next('유효하지 않는 토큰');
 
       // 토근 : undefined
-      if (user.uSeq === undefined) return next(new Error('토큰 : undefined'));
+      if (user.uSeq === undefined) return next('토큰 : undefined');
 
       socket.uSeq = user.uSeq;
       socket.uName = user.uName;
       next();
-    } catch {
-      return res.send({ error: '토큰 확인 중 서버 에러' });
+    } catch (error) {
+      console.error('토큰 확인 중 서버 에러:', error);
+      return next('토큰 확인 중 서버 에러');
     }
   },
 };
