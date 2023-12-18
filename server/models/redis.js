50dc67d814a0c98bfdf9a4e826e3295e145eb308
chitty12 <cty12angel@naver.com>
2023-12-18 15:34:14 +0900

    feat/roominfo : 채팅방클릭시 마지막메세지 로드, 메세지유효시간 설정

--- server/models/redis.js
+++ server/models/redis.js
@@ -10,14 +10,14 @@ const redisClient = redis.createClient({
 });
 
 redisClient.connect().then();
+const redisCli = redisClient.v4;
 
 redisClient.on('connect', () => {
   console.info('Redis connected!');
 });
+
 redisClient.on('error', (err) => {
   console.error('Redis Client Error', err);
 });
-
-const redisCli = redisClient.v4;
 
 module.exports = redisCli;
