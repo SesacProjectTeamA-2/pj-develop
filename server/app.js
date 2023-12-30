f0f49fab273f7c6df15817e01a8c4df6a7b83fb8
chitty12 <cty12angel@naver.com>
2023-12-30 20:13:01 +0900

    docs/app : domain 설정 전 setting 값 조정

--- server/app.js
+++ server/app.js
@@ -60,10 +60,12 @@ const options = {
     origin: [
       // 프론트
       `${process.env.SERVER_DEV_URL}:${process.env.FRONT_DEV_PORT}`, // 로컬
-      `${process.env.SERVER_PROD_DOMAIN}:${process.env.FRONT_PROD_PORT}`, // 배포
+      // `${process.env.SERVER_PROD_DOMAIN}:${process.env.FRONT_PROD_PORT}`, // 배포
+      `${process.env.SERVER_PROD_URL}:${process.env.FRONT_PROD_PORT}`, // 배포
       // 서버
       `${process.env.SERVER_DEV_URL}:${process.env.SERVER_DEV_PORT}`, // 로컬
-      `${process.env.SERVER_PROD_DOMAIN}:${process.env.SERVER_PROD_PORT}`, // 배포
+      // `${process.env.SERVER_PROD_DOMAIN}:${process.env.SERVER_PROD_PORT}`, // 배포
+      `${process.env.SERVER_PROD_URL}:${process.env.SERVER_PROD_PORT}`, // 배포
     ],
 
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
