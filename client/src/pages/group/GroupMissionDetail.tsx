4bad80c32d7ec7c3c0aa6be2cf0185ff730c35e7
saywhat <95282021+loveflora@users.noreply.github.com>
2024-01-13 07:46:49 +0900

    fix: 미션 에러 수정

--- client/src/pages/group/GroupMissionDetail.tsx
+++ client/src/pages/group/GroupMissionDetail.tsx
@@ -117,6 +117,7 @@ export default function GroupMissionDetail() {
     const [commentInput, setCommentInput] = useState({
         gbSeq,
         gbcContent: '',
+        gSeq: Number(gSeq),
     });
 
     const commentOnChange = (e: any) => {
